dab6977a3bbb0bf0037b998c19ed0c50dc781f47
CharlesKirkSD <charlesackirk@gmail.com>
2024-11-20 10:03:15 +0000

    Created 02 Getting Started

---
+++ 02 Getting Started/Getting-Started.lua
@@ -0,0 +1,8 @@
+-- Comments in Lua are indicated by double dashes, --.
+-- First program in Lua, uncomment the next line to run it.
+-- print("Hello World!!!")
+
+-- Hello World program with a for loop
+for i = 1, 10, 5 do
+  print(i .. " " .. "Hello World!!!")
+end
