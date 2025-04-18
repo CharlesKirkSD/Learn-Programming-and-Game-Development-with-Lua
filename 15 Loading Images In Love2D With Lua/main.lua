050ded2785cca7fc87126804733a4b7b3fdc3fa5
CharlesKirkSD <charlesackirk@gmail.com>
2025-04-18 09:24:58 +0100

    Updated Load Imagess in Love2D with Lua to match the YouTube video.

--- 15 Loading Images In Love2D With Lua/main.lua
+++ 15 Loading Images In Love2D With Lua/main.lua
@@ -1,11 +1,17 @@
-moveSpeed = 1
-
 function love.load()
+    -- Turn LÖVE buffering off so that anything output with print() is output immediately.
+  -- https://studio.zerobrane.com/doc-faq#why-dont-i-see-print-output-immediately-in-the-output-window
+  io.stdout:setvbuf("no")
+  
   image = love.graphics.newImage("/Assets/another-world-PREVIEW.png")
   
   -- Get the dimensions of the image
   imageWidth = image:getWidth()
   imageHeight = image:getHeight()
+  
+  -- Alternative approach using getDimensions(), a function which returns multiple (two) values.
+  -- imageWidth, imageHeight = image:getDimensions() -- Uncomment this line to use getDimensions().
+
   print(imageWidth .. "   " .. imageHeight)
 end
 
